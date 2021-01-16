ba297caf823603d1f2fe48698b75c7da6f419819
Brian Gershon <brian.five@gmail.com>
2021-01-16 14:25:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## nextjs-starter
+
+Starter template for Next.js.
+
+Features
+* Start with minimal Next.js app (using `npm`) via `npx create-next-app nextjs-starter --use-npm` then simplified example
+* Add health check endpoint
+* Add placeholder `favicon.ico`
+
+## Run locally
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>nextjs-starter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hello!
+        </h1>
+      </main>
+    </div>
+  )
+}
