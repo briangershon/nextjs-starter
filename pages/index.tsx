8fd59557be277bbcf0bac3504e44bdcc439a42b3
Brian Gershon <briangershon@users.noreply.github.com>
2022-07-29 06:46:06 -0700

    Add tailwindcss (v3.1.6) (#2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="flex h-screen">
       <Head>
         <title>nextjs-starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hello!</h1>
+      <main className="m-auto">
+        <div className="text-3xl font-bold">Hello!</div>
       </main>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
